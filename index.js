b99de12f9485d4890c7532287b0226d82650b977
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-14 15:32:41 +0300

    commit tree fd943b0f394d424287d56fba17457807ebda78d7

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const target = document.querySelector("#text");
+    target.textContent = "This is really cool!"
+});
